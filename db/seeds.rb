15806f1e853856daee7b544b53976076b0ed84b3
Kristian Soelling <kristiansoelling@gmail.com>
2019-02-15 11:36:38 +0100

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+require 'faker'
+
+# # Ingredient
+# cocktail_list = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
+# puts JSON.parse(cocktail_list)
+#
+# puts cocktail_list
+
+# Cocktail names
+
+10.times do
+  ingredient = Ingredient.new(name: Faker::Food.spice)
+  ingredient.save!
+end
+puts 'Finished!'
+
+
+
+# # Cocktail names
+# 10.times do
+#   cocktail = Cocktail.new
+#   cocktail.name = Faker::Beer.style,
+#   cocktail.doses.first.description = Faker::Food.measurement,
+#   cocktail.doses.first.ingredient_id = [1...3].sample
+#   cocktail.save!
+# end
+# puts 'Finished!'
